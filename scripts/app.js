0d60b1be6fa37bec8ab8c21e3e8c435fe792abb6
stefani spayde <spayde.stefani@gmail.com>
2017-05-11 11:59:27 -0500

    rcvd ajax and writing onSuccess function

--- scripts/app.js
+++ scripts/app.js
@@ -2,7 +2,35 @@
 var weekly_quakes_endpoint = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson";
 
 $(document).ready(function() {
-  console.log("Let's get coding!");
+  // console.log("Let's get coding!");
   // CODE IN HERE!
+function getQuakes() {
+  $.ajax({
+    method: "GET",
+    url: weekly_quakes_endpoint,
+    data: "json",
+    success: onSuccess,
+    error: onError
+  });
+}
 
+function onSuccess(json) {
+  console.log(json);
+  for (var i=0; i < json.features.length; i++) {
+	$("info").append("<p>"+json.features[i].properties.title+"</p>");
+  console.log(json.features[i].properties.title);
+}
+}
+
+function onError(xhr, status, errorThrown) {
+  alert("Sorry, there was a problem!");
+  console.log("Error: " + errorThrown);
+  console.log("Status: " + status);
+  console.dir(xhr);
+}
+
+
+
+
+ getQuakes();
 });
