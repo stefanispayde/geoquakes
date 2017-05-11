a3c27ad50a2ed03352d2705cffbd2ab099c2e1e4
stefani spayde <spayde.stefani@gmail.com>
2017-05-11 12:17:13 -0500

    added titles

--- scripts/app.js
+++ scripts/app.js
@@ -2,35 +2,35 @@
 var weekly_quakes_endpoint = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson";
 
 $(document).ready(function() {
-  // console.log("Let's get coding!");
-  // CODE IN HERE!
-function getQuakes() {
-  $.ajax({
+// c onsole.log(“Let’s get coding!“);
+ // CODE IN HERE!
+
+getQuake()
+});
+
+function getQuake() {
+ $.ajax({
     method: "GET",
     url: weekly_quakes_endpoint,
-    data: "json",
+    datatype: "json",
     success: onSuccess,
     error: onError
-  });
-}
-
-function onSuccess(json) {
-  console.log(json);
-  for (var i=0; i < json.features.length; i++) {
-	$("info").append("<p>"+json.features[i].properties.title+"</p>");
-  console.log(json.features[i].properties.title);
-}
-}
-
-function onError(xhr, status, errorThrown) {
-  alert("Sorry, there was a problem!");
-  console.log("Error: " + errorThrown);
-  console.log("Status: " + status);
-  console.dir(xhr);
-}
-
-
-
-
- getQuakes();
 });
+
+}
+
+function onSuccess(json){
+ //console.log(json);
+ // TODO: forEach (earthquake)
+ for (i = 0; i < json.features.length; i++){
+   var title = json.features[i].properties.title;
+   $("#info").append(`<p>${title}</p>`);
+
+ }
+}
+
+function onError(a, b, c){
+ console.log(a);
+ console.log(b);
+ console.log(c);
+}
