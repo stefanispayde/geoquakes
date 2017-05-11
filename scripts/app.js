685979f9a1c87f1e9e9bbdc2847b6cf53d3bd964
stefani spayde <spayde.stefani@gmail.com>
2017-05-11 16:15:42 -0500

    added pin on map for each quake

--- scripts/app.js
+++ scripts/app.js
@@ -1,45 +1,68 @@
 // define globals
 var weekly_quakes_endpoint = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson";
+var map;
 
 $(document).ready(function() {
-// c onsole.log(“Let’s get coding!“);
- // CODE IN HERE!
-initMap();
-getQuake()
-//map HERE ?key=AIzaSyBHLett8djBo62dDXj0EjCimF8Rd6E8cxg
-      var map;
-      function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: 30.2682, lng: -97.74295},
-          zoom: 8
-        });
-      }
+  // c onsole.log(“Let’s get coding!“);
+   // CODE IN HERE!
+  initMap();
+  getQuake();
+  //map HERE ?key=AIzaSyBHLett8djBo62dDXj0EjCimF8Rd6E8cxg
 
 });
 
+function initMap() {
+  var austin = {lat: 30.2682, lng: -97.74295};
+  map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 8,
+    center: austin
+  });
+
+  var marker = new google.maps.Marker({
+    position: austin,
+    map: map
+  });
+}
+
 function getQuake() {
- $.ajax({
+  $.ajax({
     method: "GET",
     url: weekly_quakes_endpoint,
+    data: $("FeatureCollection").serialize(),
     datatype: "json",
     success: onSuccess,
     error: onError
-});
-
+  });
 }
+
 
 function onSuccess(json){
- //console.log(json);
- // TODO: forEach (earthquake)
- for (i = 0; i < json.features.length; i++){
-   var title = json.features[i].properties.title;
-   $("#info").append(`<p>${title}</p>`);
+  console.log( json.features );
+  console.log( json.features[0].geometry.coordinates[1] );
+  // TODO: forEach (earthquake)
+  for (var i = 0; i < json.features.length; i++){
+    var quake = json.features[i];
+    $("#info").append('<p>${quake.properties.place}</p>');
 
- }
+    var lat = json.features[i].geometry.coordinates[1];
+    var lng = json.features[i].geometry.coordinates[0];
+    //  var coordinates = {latitude: latitude, longitude: longitude};
+
+
+    var marker = new google.maps.Marker({
+     position:
+     {
+       lat: lat,
+       lng: lng
+      },
+     map: map
+    });
+  }
 }
 
-function onError(a, b, c){
- console.log(a);
- console.log(b);
- console.log(c);
+
+function onError(xhr, status, errorThrown){
+ console.log(errorThrown);
+ console.log(status);
+ console.dir(xhr);
 }
